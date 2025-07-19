37c91b137bd77ad4d6e0fd863cab22d22500db23
herm1t <72761718+herm1t0@users.noreply.github.com>
2025-07-19 14:53:41 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,20 @@
+function Test-ScriptIntegrity {
+    $scriptUrl = https://raw.githubusercontent.com/herm1t0/win-config/refs/heads/main/install.ps1
+    $hashUrl = https://raw.githubusercontent.com/herm1t0/win-config/refs/heads/main/releaseHash
+    $releaseHash = Invoke-RestMethod -Uri $hashUrl
+    $scriptContent = Invoke-RestMethod -Uri $scriptUrl
 
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($scriptContent)
+    $sha256 = [System.Security.Cryptography.SHA256]::Create()
+    $hashBytes = $sha256.ComputeHash($bytes)
+    $hashString = [System.BitConverter]::ToString($hashBytes).Replace("-", "")
+
+    if ($releaseHash -ne $hashString) {
+        Write-Warning "Non equal"
+    }
+    else {
+        Write-Warning "Equal"
+    }
+
+
+}
